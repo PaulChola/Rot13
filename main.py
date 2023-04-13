db436efebb2ebeb7ff2780dc06388441c3ebbe3a
mumbipaul02@gmail.com <mumbipaul02@gmail.com>
2023-04-13 23:42:00 +0200

    Description: This is a Python program that allows the user to choose between two programs: ROT13 encryption/decryption and visual cryptography.
    In the ROT13 program, the user can input a message and choose between encryption or decryption mode. If encryption mode is chosen, the user is prompted to input a shift value, and each alphabetic character in the message is shifted by the given shift value. If decryption mode is chosen, the user is prompted to input the shift value used to encrypt the message, and each alphabetic character in the message is shifted by the inverse of the given shift value. Non-alphabetic characters are left as-is.
    
    In the visual cryptography program, the program loads two image files ("scrambled1.png" and "scrambled2.png"), scrambles their pixels together, and saves the resulting image as "output.png".
    
    Dependencies:
    This program requires the following modules to be installed:
    
    pyttsx3 (for text-to-speech functionality)
    PIL (for image manipulation)
    Usage:
    Run the Python program using a Python interpreter such as IDLE or the command line.
    Choose between ROT13 encryption/decryption and visual cryptography by entering "R" or "V", respectively.
    If ROT13 encryption/decryption is chosen, follow the prompts to input a message, choose a mode (encryption or decryption), and input a shift value (if encrypting).
    If visual cryptography is chosen, ensure that the two image files "scrambled1.png" and "scrambled2.png" are located in the same directory as the Python program. If not, an error message will be displayed.
    After the program has finished, the resulting encrypted/decrypted message or the output image will be displayed and/or spoken aloud (if text-to-speech is enabled).

--- main.py
+++ main.py
@@ -1,45 +1,106 @@
-# Author:   [PAUL CHOLA MUMBI]
-# Linkedin: [https://www.linkedin.com/in/paul-chola-bwembya-mumbi/]
-# Twitter:  [https://twitter.com/PaulChola96]
-# Date:     [09/04/2023]
-# Program: [rot13]
+import pyttsx3
+import sys
+from PIL import Image
 
-# Prompt user to choose encryption or decryption mode
-mode_algorithm = int(input("Enter 1 to encrypt, or 2 to decrypt: "))
 
-# Prompt user to input message to be encrypted/decrypted
-message = input("Enter a message to encrypt or decrypt: ")
+# speech library initialization
+engine = pyttsx3.init()
 
-if mode_algorithm == 1:
-    # If encryption mode is chosen, prompt user to input shift value
-    shift = int(input("Enter a shift value: "))
-    encrypted_message = ""
-    # Iterate over each character in the message and shift its ASCII value by the given shift value
-    for char in message:
-        if char.isalpha(): # Only shift alphabetic characters
-            if char.isupper(): # Handle uppercase letters
-                encrypted_message += chr((ord(char) + shift - 65) % 26 + 65)
-            else: # Handle lowercase letters
-                encrypted_message += chr((ord(char) + shift - 97) % 26 + 97)
-        else: # Leave non-alphabetic characters as-is
-            encrypted_message += char
-    print("Encrypted message:", encrypted_message)
 
-elif mode_algorithm == 2:
-    # If decryption mode is chosen, prompt user to input the shift value used to encrypt the message
-    shift = int(input("Enter the shift value used to encrypt the message: "))
-    decrypted_message = ""
-    # Iterate over each character in the message and shift its ASCII value by the inverse of the given shift value
-    for char in message:
-        if char.isalpha(): # Only shift alphabetic characters
-            if char.isupper(): # Handle uppercase letters
-                decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
-            else: # Handle lowercase letters
-                decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
-        else: # Leave non-alphabetic characters as-is
-            decrypted_message += char
-    print("Decrypted message:", decrypted_message)
+# Prompt the user to choose between ROT13 and visual cryptography
 
+program_choice = input("Choose a program: ROT13 (R) or visual cryptography (V)? ")
+
+if program_choice.upper() == "R":
+     
+    # Prompt user to choose encryption or decryption mode
+    mode_algorithm = int(input("Enter 1 for encryption mode or 2 for decryption mode: "))
+    message = input("Enter a message to encrypt or decrypt: ")
+   
+    if mode_algorithm == 1:
+        # If encryption mode is chosen, prompt user to input shift value
+        shift = int(input("Enter a shift value: "))
+        encrypted_message = ""
+
+        # Iterate over each character in the message and shift its ASCII value by the given shift value
+        for char in message:
+            if char.isalpha(): # Only shift alphabetic characters
+                if char.isupper(): # Handle uppercase letters
+                    encrypted_message += chr((ord(char) + shift - 65) % 26 + 65)
+                else: # Handle lowercase letters
+                    encrypted_message += chr((ord(char) + shift - 97) % 26 + 97)
+            else: # Leave non-alphabetic characters as-is
+                encrypted_message += char
+        engine.say("The encrypted message is " + encrypted_message)
+        print("Encrypted message:", encrypted_message)
+        engine.runAndWait()
+    
+    elif mode_algorithm == 2:
+        # If decryption mode is chosen, prompt user to input the shift value used to encrypt the message
+        shift = int(input("Enter the shift value used to encrypt the message: "))
+        decrypted_message = ""
+    
+        # Iterate over each character in the message and shift its ASCII value by the inverse of the given shift value
+        for char in message:
+            if char.isalpha(): # Only shift alphabetic characters
+                if char.isupper(): # Handle uppercase letters
+                    decrypted_message += chr((ord(char) - shift - 65) % 26 + 65)
+                else: # Handle lowercase letters
+                    decrypted_message += chr((ord(char) - shift - 97) % 26 + 97)
+            else: # Leave non-alphabetic characters as-is
+                decrypted_message += char
+        print("Decrypted message:", decrypted_message)
+        engine.say("The decrypted message is " + decrypted_message)
+        engine.runAndWait()
+
+elif program_choice.upper() == "V":
+    
+    # Load two images to scramble
+    try:
+        img1 = Image.open("scrambled1.png")
+        img2 = Image.open("scrambled2.png")
+    except FileNotFoundError:
+        error3 = "Error: could not find image files."
+        engine.say(error3)
+        print(error3)
+        engine.runAndWait()
+        sys.exit()
+    except:
+        error3 = "Error: could not open image files."
+        engine.say(error3)
+        print(error3)
+        engine.runAndWait()
+        sys.exit()
+
+    # Validate that the images have the same size
+    if img1.size != img2.size:
+        error4 = "Error: images are not the same size."
+        engine.say(error4)
+        print(error4)
+        engine.runAndWait()
+        sys.exit()
+
+    # Scramble the images
+    pixels1 = img1.load()
+    pixels2 = img2.load()
+    output_img = Image.new("RGB", img1.size)
+    output_pixels = output_img.load()
+
+    for row in range(img1.size[1]):
+        for col in range(img1.size[0]):
+            output_pixels[col,row]=(
+                (pixels1[col,row][0]+pixels2[col,row][0])%256,
+                (pixels1[col,row][1]+pixels2[col,row][1])%256,
+                (pixels1[col,row][2]+pixels2[col,row][2])%256)
+
+    # Save the output image
+    output_img.save("output.png")
+    engine.say("Visual Decryption complete")
+    engine.runAndWait()
+    
 else:
-    # If an invalid mode is chosen, display an error message
-    print("Invalid choice. Please enter 1 or 2.")
+    # Invalid choice of program
+    error2 = "Invalid message. Please enter only letters ROT13 (R) or (V) visual cryptography."
+    engine.say(f"{error2}, {print(error2)}")
+    engine.runAndWait()
+    sys.exit()
